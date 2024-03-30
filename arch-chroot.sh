44b7630218279487941b536397594f1079fee43e
DavK <davidkemp400@gmail.com>
2024-03-30 18:00:23 -0500

    Fixes

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -3,25 +3,34 @@
 mkdir /mnt
 mount -o subvol=@ /dev/nvme0n1p5 /mnt
 rm -rf /mnt/*
+
 mkdir /mnt/home
 mount -o subvol=@home /dev/nvme0n1p5 /mnt/home
 rm -rf /mnt/home/*
+
 mkdir /mnt/mnt
 mkdir /mnt/mnt/defvol
 mount -o subvol=/ /dev/nvme0n1p5 /mnt/defvol
+
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
 mount /dev/nvme0n1p2 /mnt/boot/efi
+
 mkdir /mnt/games
 mount /dev/sda2 /mnt/games
+
 mkdir /mnt/storagedrive
 mount /dev/sdb2 /mnt/storagedrive
+
 mkdir /mnt/software
 mount /dev/nvme1n1p2 /mnt/software
+
 swapon /dev/nvme0n1p6
 
 pacstrap -K /mnt amd-ucode base base-devel btrfs-progs git linux linux-firmware nano sudo
 
-cp -R . /mnt
+cp -R . /mnt/root
+
+genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
--- setup.sh
+++ setup.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+cp -R ./etc /etc
 
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc
+
 locale-gen
+
 mkinitcpio -P
+
 pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
 passwd
 useradd -m -g users -G wheel,storage,games,power,lp -s /bin/bash davk
 passwd davk
+
 EDITOR=nano visudo
+
 pacman -Sy dhcpcd
 systemctl enable dhcpcd@enp6s0
 pacman -S networkmanager
 systemctl enable NetworkManager
+
+sudo su davk
+cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -22,3 +32,4 @@ cd ..
 rm -rf yay
 yay -S linux-headers nvidia-dkms qt5-wayland qt5ct libva libva-nvidia-driver-git
 
+git clone https://github.com/BosscasDavK/HyprV4.git
